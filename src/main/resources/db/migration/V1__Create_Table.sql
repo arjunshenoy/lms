34a638ee7bbcb93217cb6cbb86599fb9fc4439ee
ajinpm <36944267+ajinpm@users.noreply.github.com>
2021-10-19 15:03:53 +0100

    Changes

--- src/main/resources/db/migration/V1__Create_Table.sql
+++ src/main/resources/db/migration/V1__Create_Table.sql
@@ -4,3 +4,13 @@ CREATE TABLE IF NOT EXISTS `login` (
     `created_ts` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY(`employee_id`)
 );
+
+CREATE TABLE IF NOT EXISTS `notification` (
+    `notification_id` INT NOT NULL,
+    `employee_id`INT NOT NULL,
+    `message` VARCHAR(30) NOT NULL,
+    `is_read` BOOLEAN DEFAULT FALSE,
+    `title` VARCHAR(30) NOT NULL,
+    `date` DATE,
+    PRIMARY KEY(`notification_id`)
+);
