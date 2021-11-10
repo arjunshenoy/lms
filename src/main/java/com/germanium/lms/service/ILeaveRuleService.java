87f26475842a107ecfb467845d83960580da3eee
Teena-Joseph-MEC <teenajoseph.mec@gmail.com>
2021-11-10 17:32:02 +0000

    adding leave Rule - leave type request

---
+++ src/main/java/com/germanium/lms/service/ILeaveRuleService.java
@@ -0,0 +1,5 @@
+package com.germanium.lms.service;
+
+public interface ILeaveRuleService {
+
+}
---
+++ src/main/java/com/germanium/lms/serviceImpl/LeaveRuleServiceImpl.java
@@ -0,0 +1,31 @@
+package com.germanium.lms.serviceImpl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.germanium.lms.model.LeaveRules;
+import com.germanium.lms.model.LeaveStats;
+import com.germanium.lms.repository.ILeaveRulesRepository;
+import com.germanium.lms.repository.ILeaveStatisticsRepository;
+
+public class LeaveRuleServiceImpl {
+	@Autowired
+	ILeaveRulesRepository leaveRulesRepo;
+
+	@Autowired
+	ILeaveStatisticsRepository leaveStatsRepo;
+
+	public boolean checkLeaveTypeRequestedForUserId(String leaveType, Integer userId) throws Exception {
+		LeaveRules leaveDetails = leaveRulesRepo.findByName(leaveType);
+		if (leaveDetails == null) {
+			throw new Exception("Leave with leave name :" + leaveType + " not found");
+		}
+		LeaveStats leaveStats = leaveStatsRepo.findLeaveTypeByUserIdAndLeaveId(leaveDetails.getLeaveId(), userId);
+		if (leaveStats == null) {
+			return false;
+		} else {
+			return true;
+		}
+
+	}
+
+}
