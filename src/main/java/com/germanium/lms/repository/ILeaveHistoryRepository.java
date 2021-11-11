3d55e00a6af96c69f9852592058cad5cc6db9e7c
ajinpm <36944267+ajinpm@users.noreply.github.com>
2021-11-11 10:00:29 +0000

    Added combinable leaves support

--- src/main/java/com/germanium/lms/repository/ILeaveHistoryRepository.java
+++ src/main/java/com/germanium/lms/repository/ILeaveHistoryRepository.java
@@ -1,8 +1,12 @@
 package com.germanium.lms.repository;
 
 import java.util.Date;
+<<<<<<< HEAD
 import java.util.List;
 import java.util.Optional;
+=======
+
+>>>>>>> e67e105 (Added combinable leaves support)
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
@@ -14,6 +18,7 @@ import com.germanium.lms.model.LeaveHistoryId;
 @Repository
 public interface ILeaveHistoryRepository extends CrudRepository<LeaveHistory, LeaveHistoryId> {
 
+<<<<<<< HEAD
 	Optional<LeaveHistory> findByLeaveId(Integer leaveId);
 
 	Optional<LeaveHistory> findByLeaveHistoryIdLeaveRequestId(Integer leaveRequestId);
@@ -22,3 +27,8 @@ public interface ILeaveHistoryRepository extends CrudRepository<LeaveHistory, Le
 	List<LeaveHistory> findClashingLeaves(@Param("date") Date checkDate);
 
 }
+=======
+	@Query("SELECT leaveHistory FROM LeaveHistory leaveHistory where leaveHistory.FromDate = :fromDate AND leaveHistory.LeaveStatus")
+	LeaveHistory findByFromDateAndIsApproved(Date fromDate, String leaveStatus);
+}
+>>>>>>> e67e105 (Added combinable leaves support)
