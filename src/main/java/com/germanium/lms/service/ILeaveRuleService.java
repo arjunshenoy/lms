353adebe4bb48d555065bb4cf9fea2768b2db489
Teena-Joseph-MEC <teenajoseph.mec@gmail.com>
2021-11-17 14:30:43 +0000

    Changes

--- src/main/java/com/germanium/lms/service/ILeaveRuleService.java
+++ src/main/java/com/germanium/lms/service/ILeaveRuleService.java
@@ -1,5 +1,7 @@
 package com.germanium.lms.service;
 
 public interface ILeaveRuleService {
+	
 	public boolean checkLeaveTypeRequestedForUserId(String leaveType, Integer userId) throws Exception;
+	
 }
