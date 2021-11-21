c2aa0eb24c45339d7ff214338969456a41ed0eac
ajinpm <36944267+ajinpm@users.noreply.github.com>
2021-11-21 00:08:54 +0000

    Added singleton logger

--- src/main/java/com/germanium/lms/controllers/LeaveController.java
+++ src/main/java/com/germanium/lms/controllers/LeaveController.java
@@ -1,6 +1,7 @@
 package com.germanium.lms.controllers;
 
 import java.util.List;
+import java.util.logging.Level;
 
 import javax.validation.Valid;
 
@@ -25,6 +26,7 @@ import com.germanium.lms.model.ActiveLeaves;
 import com.germanium.lms.model.LeaveRules;
 import com.germanium.lms.model.LeaveStats;
 import com.germanium.lms.model.dto.LeaveRequestDto;
+import com.germanium.lms.model.dto.Log;
 import com.germanium.lms.model.factory.Leave;
 import com.germanium.lms.model.factory.LeaveFactory;
 import com.germanium.lms.service.ILeaveService;
@@ -33,32 +35,36 @@ import com.germanium.lms.service.ILeaveService;
 @RequestMapping(value = "/api/v1/leave", produces = MediaType.APPLICATION_JSON_VALUE)
 public class LeaveController {
 
-	Logger logger = LoggerFactory.getLogger(LeaveController.class);
-
 	@Autowired
 	ILeaveService leaveService;
 
 	@GetMapping("leaveType")
-	public ResponseEntity<List<LeaveRules>> getLeaveRules() {
+	public ResponseEntity<List<LeaveRules>> getLeaveRules() throws Exception {
+		Log log = Log.getInstance();
+		log.logger.info("Request for fetching leave rules received");
 		return ResponseEntity.ok().body(leaveService.getLeaveRules());
 	}
 
 	@GetMapping("leaveType/{leaveId}")
-	public ResponseEntity<LeaveRules> getLeavesById(@PathVariable("leaveId") Integer leaveId) throws ResourceNotFoundException {
+	public ResponseEntity<LeaveRules> getLeavesById(@PathVariable("leaveId") Integer leaveId){
+		Log log = Log.getInstance();
+		log.logger.info("Request for fetching leaves for ID" + leaveId +  "received");
 		return ResponseEntity.ok().body(leaveService.findLeavesById(leaveId));
-
 	}
 
 	@PostMapping("leaveType")
-	public ResponseEntity<List<LeaveRules>> createLeaveRules(@Valid @RequestBody List<LeaveRules> leaveType) {
+	public ResponseEntity<List<LeaveRules>> createLeaveRules(@Valid @RequestBody List<LeaveRules> leaveType) throws Exception {
+		Log log = Log.getInstance();
+		log.logger.info("Request for adding new leave received");
 		List<LeaveRules> leaveTypeDetails = leaveService.createLeaveRules(leaveType);
 		return ResponseEntity.status(HttpStatus.CREATED).header(HttpHeaders.LOCATION).body(leaveTypeDetails);
 	}
 
 	@PutMapping("leaveType/{leaveId}")
 	public ResponseEntity<?> updateLeaveRules(@PathVariable("leaveId") final Integer leaveTypeId,
-			@Valid @RequestBody LeaveRules leaveRule) throws ResourceNotFoundException {
-
+			@Valid @RequestBody LeaveRules leaveRule) throws Exception {
+		Log log = Log.getInstance();
+		log.logger.info("Request for updating leave rules received");
 		leaveService.updateLeaveRules(leaveTypeId, leaveRule);
 		return ResponseEntity.status(HttpStatus.OK).header(HttpHeaders.LOCATION)
 				.body("Leave type Updated Successfully");
@@ -66,20 +72,24 @@ public class LeaveController {
 
 	@DeleteMapping(value = "leaveType/{leaveId}")
 	public ResponseEntity<?> deleteLeaveRules(@PathVariable("leaveId") Integer leaveId) throws ResourceNotFoundException {
-		logger.info("Delete request received for leave ID : {}", leaveId);
+		Log log = Log.getInstance();
+		log.logger.info("Delete request received for leave ID :" + leaveId);
 		return ResponseEntity.status(HttpStatus.OK).header(HttpHeaders.LOCATION)
 				.body(leaveService.deleteLeaveRules(leaveId));
 	}
 
 	@GetMapping("leaveStats/{employeeId}")
-	public ResponseEntity<List<LeaveStats>> getLeaveStatsById(@PathVariable("employeeId") Integer employeeId) {
-		logger.info("Fetching Leave Stats details for employee Id: {}", employeeId);
+	public ResponseEntity<List<LeaveStats>> getLeaveStatsById(@PathVariable("employeeId") Integer employeeId) throws Exception {
+		Log log = Log.getInstance();
+		log.logger.info("Fetching Leave Stats details for employee Id:" + employeeId);
 		List<com.germanium.lms.model.LeaveStats> lstats = leaveService.getLeaveStatsById(employeeId);
 		return ResponseEntity.ok().body(lstats);
 	}
 
 	@PostMapping("leaveStats/{userId}")
-	public void addLeaveStatsForNewUsers(@PathVariable("userId") final Integer userId) {
+	public void addLeaveStatsForNewUsers(@PathVariable("userId") final Integer userId) throws Exception {
+		Log log = Log.getInstance();
+		log.logger.info("Adding leave stats for new Users");
 		leaveService.addLeaveStatsForNewUsers(userId);
 
 	}
@@ -114,7 +124,8 @@ public class LeaveController {
 	@PostMapping("cancelRequest/{leaveRequestId}/{cancelDecision}")
 	public ResponseEntity<Boolean> cancelWithdrawLeave(@PathVariable("leaveRequestId") Integer leaveRequestId,
 			@PathVariable("cancelDecision")  String cancelDecision) {
-		logger.info("Request received for {} leave",cancelDecision);
+		Log log = Log.getInstance();
+		log.logger.info("Request received for" + cancelDecision +" leave");
 		try {
 			return ResponseEntity.ok().body(leaveService.cancelWithdrawLeave(leaveRequestId, cancelDecision));
 		} catch (Exception e) {
---
+++ src/main/java/com/germanium/lms/model/dto/Log.java
@@ -0,0 +1,43 @@
+package com.germanium.lms.model.dto;
+
+import java.io.File;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
+public class Log {
+	public Logger logger;
+	FileHandler handler;
+	private static final Log instance = new Log();
+	public String fileName = "log";
+
+	public static Log getInstance() {
+		return instance;
+	}
+
+	private Log() {
+		if (instance != null) {
+			// Prevent Reflection
+			throw new IllegalStateException("Cannot instantiate a new singleton instance of log");
+		}
+		this.createLogFile();
+	}
+
+	private void createLogFile() {
+		try {
+			File f = new File(fileName);
+			if (!f.exists()) {
+				f.createNewFile();
+			}
+			handler = new FileHandler(fileName, true);
+			logger = Logger.getLogger("lms");
+			logger.setLevel(Level.ALL);
+			logger.addHandler(handler);
+			SimpleFormatter formatter = new SimpleFormatter();
+			handler.setFormatter(formatter);
+		} catch (Exception e) {
+
+		}
+	}
+}
