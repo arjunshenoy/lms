968564f8570bfe4db4cac2de69f474b22f33fb90
import-cj <josechinmay@gmail.com>
2022-03-24 11:42:57 +0000

    update messages

---
+++ src/main/java/com/germanium/lms/serviceImpl/NotifyLeaveHistory.java
@@ -0,0 +1,33 @@
+package com.germanium.lms.serviceImpl;
+
+import java.util.Optional;
+
+import com.germanium.lms.model.ActiveLeaves;
+import com.germanium.lms.model.LeaveHistory;
+import com.germanium.lms.service.bridge.ISendAPI;
+import com.germanium.lms.service.bridge.Notifier;
+
+public class NotifyLeaveHistory extends Notifier {
+	String status;
+	Optional<ActiveLeaves> optionalLeave;
+	Optional<LeaveHistory> optionalLeaveHistory;
+    public NotifyLeaveHistory(String sub, ISendAPI sender, Optional<ActiveLeaves> optionalLeave, Optional<LeaveHistory> optionalLeaveHistory, String status) {
+        super(sub, sender);
+        this.optionalLeave = optionalLeave;
+        this.optionalLeaveHistory = optionalLeaveHistory;
+        this.status = status;
+    }
+    
+    
+	public String buildMsg() {
+		int id = optionalLeave!=null ? optionalLeave.get().getEmployeeId() : optionalLeaveHistory.get().getLeaveHistoryId().getEmployeeId();
+		String content = "Leave Application Request by " + id;
+		content += " is " + this.status+ "\n\n";
+		content += "Details :\n";
+		content += "Leave type :"+ optionalLeave!=null ? optionalLeave.get().getLeaveId() : optionalLeaveHistory.get().getLeaveId() + "\n";
+		content += "Leave Starts on :"+ optionalLeave!=null ? optionalLeave.get().getFromDate() : optionalLeaveHistory.get().getFromDate()+"\n";
+		content += "Leave Ends on :"+optionalLeave!=null ? optionalLeave.get().getToDate() : optionalLeaveHistory.get().getToDate()+"\n";
+		return content;
+	}
+}
+
