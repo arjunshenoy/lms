ad8a5f477cbf776081aa918c17ccdf697cd9a5b3
import-cj <josechinmay@gmail.com>
2022-03-29 23:51:35 +0100

    try fix

--- src/main/java/com/germanium/lms/serviceImpl/Mailer.java
+++ src/main/java/com/germanium/lms/serviceImpl/Mailer.java
@@ -3,15 +3,22 @@ package com.germanium.lms.serviceImpl;
 import com.germanium.lms.model.dto.MailRequestDto;
 import com.germanium.lms.service.bridge.ISendAPI;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-
+@Service
 public class Mailer implements ISendAPI {
 	private String userService;
+	@Autowired
 	private RestTemplate restTemplate;
 	MailRequestDto mailRequest;
 	
 	private String endPoint;
+
+	public Mailer() {
+
+	}
 
 	public Mailer(int employeeId, String userService, String endPoint) {
 		mailRequest = new MailRequestDto();
