31c6905fa6818465f42412b918068d54af14c467
ajinpm <36944267+ajinpm@users.noreply.github.com>
2022-04-27 21:56:47 +0100

    Changes

--- src/main/java/com/germanium/lms/serviceImpl/LeaveUtilServiceImpl.java
+++ src/main/java/com/germanium/lms/serviceImpl/LeaveUtilServiceImpl.java
@@ -3,6 +3,7 @@ package com.germanium.lms.serviceImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.germanium.lms.model.factory.Leave;
 import com.germanium.lms.repository.ILeaveHistoryRepository;
@@ -10,6 +11,7 @@ import com.germanium.lms.service.ILeaveUtilService;
 import com.germanium.lms.service.adapter.ITarget;
 import com.germanium.lms.service.decorator.IAutoApprove;
 
+@Service
 public class LeaveUtilServiceImpl implements ILeaveUtilService {
 
 	Logger logger = LoggerFactory.getLogger(LeaveUtilServiceImpl.class);
